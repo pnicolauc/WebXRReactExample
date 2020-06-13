bf5c7e54d3225b8ccea347928d8a576a39cb0278
Pedro Carvalho <80.nicolau@gmail.com>
2020-06-13 21:29:22 +0100

    [Feat] Add React-XR example
    Add React XR example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
+import { Hover, Select, DefaultXRControllers, ARCanvas } from "react-xr";
 import "./styles.css";
 
-export default function App() {
+function Box({ color, size, scale, children, ...rest }) {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <mesh scale={scale} {...rest}>
+      <boxBufferGeometry attach="geometry" args={size} />
+      <meshPhongMaterial attach="material" color={color} />
+      {children}
+    </mesh>
   );
 }
+
+function Button(props) {
+  const [hover, setHover] = useState(false);
+  const [color, setColor] = useState("blue");
+
+  const onSelect = useCallback(() => {
+    setColor((Math.random() * 0xffffff) | 0);
+  }, [setColor]);
+
+  return (
+    <Select onSelect={onSelect}>
+      <Hover onChange={setHover}>
+        <Box
+          color={color}
+          scale={hover ? [0.6, 0.6, 0.6] : [0.5, 0.5, 0.5]}
+          size={[0.4, 0.1, 0.1]}
+          {...props}
+        >
+          Hello react-xr!
+        </Box>
+      </Hover>
+    </Select>
+  );
+}
+
+function App() {
+  return (
+    <ARCanvas sRGB>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Button position={[0, 0.1, -0.2]} />
+      <DefaultXRControllers />
+    </ARCanvas>
+  );
+}
+
+export default App;
